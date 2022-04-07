402178a215c869165ca2e098d8cf127bf0751631
Svetlana Pozina <svt_1108@inbox.ru>
2022-04-07 20:42:35 +0300

    init: start shelter

---
+++ shelter/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+const sassloader = require("sass-loader");
+const htmlloader = require("html-webpack-plugin");
+const miniCss = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [miniCss.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new miniCss({
+      filename: "style.css",
+    }),
+  ],
+};
