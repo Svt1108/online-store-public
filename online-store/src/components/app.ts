29f7aa1dbc6c9419181d1f76b7383d4cd5c7726d
svt1108 <svt_1108@inbox.ru>
2022-07-09 23:15:40 +0300

    feat: improve interface

--- online-store/src/components/app.ts
+++ online-store/src/components/app.ts
@@ -15,7 +15,23 @@ class App {
   }
 
   public start(): void {
-    // console.log(platesArray);
+    //   if (!localStorage.getItem('author_saved')) localStorage.setItem('author_saved', 'light');
+
+    const author = document.getElementById("author") as HTMLElement;
+    author.addEventListener("click", (event: Event) => {
+      if (event.target !== author) {
+        if (!localStorage.getItem("author_saved")) {
+          const author_saved = [(<HTMLElement>event.target).textContent];
+          localStorage.setItem("author_saved", JSON.stringify(author_saved));
+        } else {
+          const author_saved: Array<string> = JSON.parse(
+            localStorage.getItem("author_saved") as string
+          );
+        }
+      }
+      //      console.log((<HTMLElement>event.target).textContent);
+    });
+
     this.cardList.drawCards(this.data);
     this.filterList.setFilters(this.data);
   }
